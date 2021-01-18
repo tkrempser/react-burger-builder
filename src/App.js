10b0c489196247536e7f9908b58dc0979db50a27
Thiago Krempser <tkrempser@gmail.com>
2021-01-18 15:20:09 -0300

    Update app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import React, {Component} from "react";
+
+import Layout from './components/Layout/Layout';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
