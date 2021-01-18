29241bd959812a3a87a9f6b6667ca244203054aa
Thiago Krempser <tkrempser@gmail.com>
2021-01-18 16:36:12 -0300

    Update layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Aux from  '../../hoc/Aux';
+import styles from './Layout.module.css';
 
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, Drawer, Backdrop</div>
-    <main>
+    <main className={styles.content}>
       {props.children}
     </main>
   </Aux>
