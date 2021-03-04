5fba2d6e7bbf6753042c0b91109c811d585c140e
Thiago Krempser <tkrempser@gmail.com>
2021-03-04 14:03:11 -0300

    Add price indicator

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,14 +36,36 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
-    // TODO
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0)
+      return;
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ingredients: updatedIngredients, totalPrice: newPrice});
   }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls 
+          ingredientAdded={this.addIngredientHandler} 
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+        />
       </Aux>
     );
   }
